4c19c8ce1318dba556e4a5ad91445363a136cb0c
yoonho <yoonho0922@naver.com>
2020-07-31 15:22:13 +0900

    설명 추가

--- study/boostcourse-ml-tensorflow/simple_linear_regression.py
+++ study/boostcourse-ml-tensorflow/simple_linear_regression.py
@@ -1,20 +1,34 @@
 import tensorflow as tf
 #tf.enable_eager_execution()
 
+# 데이터 입력
 x_data = [1,2,3,4,5]
 y_data = [1,2,3,4,5]
 
+# weight와 bias 무작위로 초기화
 W = tf.Variable(2.9)
 b = tf.Variable(0.5)
 
-learning_rage = 0.01
+# learning rate 설정
+learning_rate = 0.01
 
-for i in range(100+1):
+# epoch 설정
+epoch = 100
+
+# 학습
+for i in range(epoch+1):
+    # GradeintTape : tape에 context 안에서 실행된 모든 연산을 tape에 "기록"
     with tf.GradientTape() as tape:
         hypothesis = W * x_data + b
         cost = tf.reduce_mean(tf.square(hypothesis - y_data))
-    W_grad, b_grad = tape.gradient(cost, [W,b])
-    W.assign_sub(learning_rage * W_grad)
-    b.assign_sub(learning_rage * b_grad)
+    # tape.gradeint 기록된 연산의 "그래디언트"를 계산
+    # W_grad, b_grad = tape.gradeint(cost, [W,b])
+    W_grad = tape.gradient(cost, W)
+    b_grad = tape.gradient(cost, b)
+
+    # 업데이트 : 그래디언트가 0이 되도록
+    W.assign_sub(learning_rate * W_grad)
+    b.assign_sub(learning_rate * b_grad)
+
     if i % 10 == 0:
         print("{:5}|{:10.4f}|{:10.4}|{:10.6f}".format(i, W.numpy(), b.numpy(), cost))
