78121d43f7fed423c75eecf594a93c7abcd47181
yoonho <yoonho0922@naver.com>
2020-08-03 10:31:12 +0900

    add xor

--- study/boostcourse-ml-tensorflow/multiple_linear_regression.py
+++ study/boostcourse-ml-tensorflow/multiple_linear_regression.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+# 데이터 입력
 data = np.array([
     #X1,  X2,  X3,  y
     [73., 80., 75., 152.],
@@ -9,31 +10,30 @@ data = np.array([
     [96., 98., 100., 196.],
     [73., 66., 70., 142.]
 ], dtype=np.float32)
-
 X = data[:, :-1]
 y = data[:, [-1]]
 
-#initialize W, b
+# weight와 bias 무작위로 초기화
 W = tf.Variable(tf.random.normal([3,1]))
 b = tf.Variable(tf.random.normal([1]))
 
+# learning rate 설정
 learning_rate = 0.000001
 
+# 계산한 n * 1 행렬 반환
 def predict(X):
     return tf.matmul(X, W) + b
 
-n_epochs = 1000
+n_epochs = 10000
 for i in range(n_epochs+1):
-    # record the gradient of the cost function
     with tf.GradientTape() as tape:
         cost = tf.reduce_mean((tf.square(predict(X)-y)))
 
-    # calculates the gradient of the loss
+    # W_grad : 3 * 1 행렬
     W_grad, b_grad = tape.gradient(cost, [W, b])
 
-    # updates parameters (W and b)
     W.assign_sub(learning_rate * W_grad)
     b.assign_sub(learning_rate * b_grad)
 
-    if i % 100 == 0:
+    if i % 1000 == 0:
         print("{:5}|{:10.4f}|".format(i, cost.numpy()))
